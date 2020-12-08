f97121cf37b2cf4fd6bf4f87f6c607dd90850bc9
epaschke22 <ericksonpaschke@gmail.com>
2020-12-08 19:50:44 +0000

    submitting task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    str = ""
+    for i in range(1, 101):
+        if i % 3 == 0:
+            str += "Fizz"
+        if i % 5 == 0:
+            str += "Buzz"
+        if str != "":
+            print(str, end='')
+            str = ""
+        else:
+            print(i, end='')
+        print(" ", end='')
+    print("")
