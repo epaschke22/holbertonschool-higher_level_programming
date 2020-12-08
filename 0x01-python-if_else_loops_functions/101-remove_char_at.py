ff082a2ca5e93f2ff6f6716ea4f9a938907b06fe
epaschke22 <ericksonpaschke@gmail.com>
2020-12-08 22:54:12 +0000

    updating task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,9 +3,3 @@ def remove_char_at(str, n):
     if n < 0:
         return str
     return str[:n] + str[n + 1:]
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
