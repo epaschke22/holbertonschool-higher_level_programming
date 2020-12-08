d9045983b2ddd3da3bd9cb172375b4edfe2e8bea
epaschke22 <ericksonpaschke@gmail.com>
2020-12-08 20:35:00 +0000

    updating task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    str = ""
+    result = ""
+    fb = ""
     for i in range(1, 101):
         if i % 3 == 0:
-            str += "Fizz"
+            fb += "Fizz"
         if i % 5 == 0:
-            str += "Buzz"
-        if str != "":
-            print(str, end='')
-            str = ""
+            fb += "Buzz"
+        if fb != "":
+            result += fb
+            fb = ""
         else:
-            print(i, end='')
-        print(" ", end='')
-    print("")
+            result += str(i)
+        result += " "
+    print(result)
+
+fizzbuzz()
