27a4d1eecafe0a3131022b66752bdcfbd8aca87c
epaschke22 <ericksonpaschke@gmail.com>
2020-12-08 19:17:15 +0000

    submitting task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        char = ord(i)
+        if char > 96:
+            char -= 32
+        print("{:c}".format(char), end='')
+    print("")
