f4b45ef8f47cdfbe2987e0b5d7a77282dee17946
epaschke22 <ericksonpaschke@gmail.com>
2020-12-10 18:23:26 +0000

    updating requirements on task 0 through 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) < 2:
-    print("0 arguments")
-else:
-    count = -1
-    args = len(sys.argv) - 1
-    print("{} argument{}".format(args, "s" if args != 1 else ""))
-    for i in sys.argv:
-        if count == -1:
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("0 arguments")
+    else:
+        count = -1
+        args = len(sys.argv) - 1
+        print("{} argument{}".format(args, "s" if args != 1 else ""))
+        for i in sys.argv:
+            if count == -1:
+                count += 1
+                continue
+            print("{}: {}".format(count, i))
             count += 1
-            continue
-        print("{}: {}".format(count, i))
-        count += 1
