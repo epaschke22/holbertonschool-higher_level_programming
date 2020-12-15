1c3c0f15bc31409f93dcf3ac96ccd5dd9dc138b6
epaschke22 <ericksonpaschke@gmail.com>
2020-12-15 18:42:02 +0000

    submitting task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1:
+        tupa = (tuple_a[0], 0)
+    elif len(tuple_a) == 0:
+        tupa = (0, 0)
+    else:
+        tupa = tuple_a
+    if len(tuple_b) == 1:
+        tupb = (tuple_b[0], 0)
+    elif len(tuple_b) == 0:
+        tupb = (0, 0)
+    else:
+        tupb = tuple_b
+
+    int1 = tupa[0] + tupb[0]
+    int2 = tupa[1] + tupb[1]
+    new_tuple = (int1, int2)
+    return new_tuple
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
