b503e6865bb6012979566af7458d70a37128602c
epaschke22 <ericksonpaschke@gmail.com>
2020-12-15 21:30:11 +0000

    submitting task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in linked
+ * @h: list to prtinf from
+ * Return: number of elements
+ */
+size_t list_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: input list
+ * Return: 1 if palindrome, else 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *fronthead = *head, *backhead;
+	size_t i, len1 = 0, len2 = 0;
+
+	if (head == NULL)
+		return 0;
+	if (*head == NULL)
+		return (1);
+    if (fronthead->next == NULL)
+        return (1);
+    len2 = list_len(fronthead) - 1;
+
+    while (len2 >= len1)
+    {
+        fronthead = *head;
+        for (i = 0; i < len1; i++)
+		{
+            fronthead = fronthead->next;			
+		}
+        backhead = *head;
+        for (i = 0; i < len2; i++)
+		{
+            backhead = backhead->next;			
+		}
+        if (fronthead->n != backhead->n)
+            return (0);
+        len1++;
+        len2--;
+    }
+    return (1);
+}
