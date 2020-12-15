94262d43e967e91ef373994ae0757a6a7dd4a6ed
epaschke22 <ericksonpaschke@gmail.com>
2020-12-15 19:17:48 +0000

    updaitng task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return 0, None
+    if len(sentence) is 0:
+        return 0, ""
     length = len(sentence)
     first = sentence[0]
     return (length, first)
+
+sentence = ""
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
