6796165ff31d83c4d92b91bc07a823c9ba16e627
epaschke22 <ericksonpaschke@gmail.com>
2020-12-17 21:54:51 +0000

    updating task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if cond is None:
+    if a_dictionary is None:
         return None
     output = -10000000000
     for key, value in a_dictionary.items():
         if value > output:
             output = value
     return output
+    
