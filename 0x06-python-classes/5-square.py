e77eae2e57e7e8142ad511bd942be5e1c5d52062
epaschke22 <ericksonpaschke@gmail.com>
2021-01-05 20:34:43 +0000

    submitting task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This is a example class"""
+
+
+class Square:
+    """This is a class with a private feild"""
+    def __init__(self, size=0):
+        """This initializes the size feild"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter function"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This method finds the square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints out a square"""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
