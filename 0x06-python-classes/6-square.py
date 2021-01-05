7ea04962015c0da0e54185d8c1e3f48972ce6f14
epaschke22 <ericksonpaschke@gmail.com>
2021-01-05 22:11:57 +0000

    updating task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,8 +51,10 @@ class Square:
         if self.__size == 0:
             print("")
         else:
+            for i in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
-                for k in range(self.__position[0]):
+                for j in range(self.__position[0]):
                         print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
