bd0e200797d534dda4a0eea2af97dcd0ba625d89
epaschke22 <ericksonpaschke@gmail.com>
2021-01-08 18:09:43 +0000

    submitting all python files for task 0 to 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """checks to make sure input is string and replaces characters"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    t = text.replace(".", "\n\n").replace("?", "\n\n").replace(":", "\n\n")
+    print(t, end="")
