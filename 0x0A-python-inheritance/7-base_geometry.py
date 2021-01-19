71634b1c75b171e4c417efece3b3ddc490b6f8f1
epaschke22 <ericksonpaschke@gmail.com>
2021-01-19 21:18:21 +0000

    updating tasks with doc tests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         """raises exception when not implemented"""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name="", value=0):
         """makes sure the input is correct"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
