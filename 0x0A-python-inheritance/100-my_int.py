06175ee0ca8eef9be0959e373e4d9e47aff37645
epaschke22 <ericksonpaschke@gmail.com>
2021-01-19 20:30:44 +0000

    updating task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ class MyInt(int):
     """New version of int"""
     def __eq__(self, other):
         """check if equal to other"""
-        return False
+        return int.__ne__(self, other)
 
     def __ne__(self, other):
         """check if not equal to other"""
-        return True
+        return int.__eq__(self, other)
