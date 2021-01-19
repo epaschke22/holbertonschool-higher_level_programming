959851850592e8c75301abf97dcae696969b1ebc
epaschke22 <ericksonpaschke@gmail.com>
2021-01-19 23:35:23 +0000

    took out main code in 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,8 +14,3 @@ class Square(Rectangle):
     def __str__(self):
         """returns string when printed"""
         return "[Square] {}/{}".format(self.__size, self.__size)
-
-s = Square(13)
-
-print(s)
-print(s.area())
