8bae223d225b659afe3a72953f4826c2641bf0c5
epaschke22 <ericksonpaschke@gmail.com>
2021-01-20 01:37:39 +0000

    updating task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function that adds attributes only if they exist"""
+
+
 def add_attribute(obj, name, value):
+    """checks for object first"""
     if hasattr(obj, "__dict__") is False:
         raise TypeError("can't add new attribute")
     else:
