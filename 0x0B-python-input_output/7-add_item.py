3c765ab39c4d5037bf10773b9b5390a53baa5567
epaschke22 <ericksonpaschke@gmail.com>
2021-01-21 05:02:55 +0000

    updtating task 7 8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Load, add, save """
-from sys import argv
 
+
+from sys import argv
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
