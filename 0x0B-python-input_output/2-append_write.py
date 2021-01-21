d6b01c6dbe55e6d159e9668177f23bafc591b771
epaschke22 <ericksonpaschke@gmail.com>
2021-01-21 05:10:45 +0000

    updating task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Append to a file"""
 
 
-def write_file(filename="", text=""):
+def append_file(filename="", text=""):
     """appends text to file at the end and returns bytes written"""
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
