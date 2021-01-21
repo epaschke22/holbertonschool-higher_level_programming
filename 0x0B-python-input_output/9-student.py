72753a8f13be27c813295185d5d096e07f3dcf6c
epaschke22 <58716746+epaschke22@users.noreply.github.com>
2021-01-21 00:05:44 -0500

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,14 +13,4 @@ class Student:
     def to_json(self):
         """returns dictionary of class to json"""
         return vars(self)
-
-
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+    
