6b6ea837e7a98aba74fc696bdab14109984dc0a4
epaschke22 <ericksonpaschke@gmail.com>
2021-01-27 02:49:06 +0000

    updating base test for json

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -27,3 +27,13 @@ class TestBaseInit(unittest.TestCase):
         b5 = Base(12)
         self.assertEqual(b5.id, 12)
 
+    def test_json(self):
+        """tests to_json methon in Base"""
+        r1 = Rectangle(10, 7, 2, 8)
+        s1 = Square(4, 2, 3)
+        dictionary1 = r1.to_dictionary()
+        dictionary2 = s1.to_dictionary()
+        json_dictionary = Rectangle.to_json_string([dictionary1, dictionary2])
+        list_dict = Rectangle.from_json_string(json_dictionary)
+        self.assertDictEqual(list_dict[0], dictionary1)
+        self.assertDictEqual(list_dict[1], dictionary2)
