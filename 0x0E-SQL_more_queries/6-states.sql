58ab5e7b170d6868c460b785efd05e7a748d5ee6
epaschke22 <ericksonpaschke@gmail.com>
2021-03-02 19:09:55 +0000

    submitting task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates database for USA with table of States
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
