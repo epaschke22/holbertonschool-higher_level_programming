912cbcabea8ebaefcb2d4be8b5669e312922accf
epaschke22 <ericksonpaschke@gmail.com>
2021-03-02 19:47:07 +0000

    updating task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- Lists all the cities of Calafornia
-SELECT * FROM cities WHERE stat_id = (
-SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE stat_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
