fdf4911fe56dd5f760c38da5f7b8550b5a9cd83c
epaschke22 <ericksonpaschke@gmail.com>
2021-03-02 21:49:35 +0000

    submitting task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists genres and number of TV shows that match
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres LEFT JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.show_id
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP By tv_genres.name
 ORDER BY number_of_shows DESC;
