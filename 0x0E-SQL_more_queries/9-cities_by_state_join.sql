614e5b48dc41745c1e0eb7c3e01e6fc01e96e086
epaschke22 <ericksonpaschke@gmail.com>
2021-03-02 20:09:20 +0000

    updating task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Displays all the cities followed by their states
-SELECT id, cities.name, state.name FROM cities
+SELECT cities.id, cities.name, state.name FROM cities
 LEFT JOIN states ON cities.state_id = states.name;
