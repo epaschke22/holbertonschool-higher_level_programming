39f1691cd8c07b43ce30e97195910d143569c01b
epaschke22 <ericksonpaschke@gmail.com>
2021-03-02 20:47:04 +0000

    updating task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- Join tv shows with their grenes
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
