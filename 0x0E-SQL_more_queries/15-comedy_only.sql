e6c06f7b260a5f5e5506317bbe750dd5a11b15ca
epaschke22 <ericksonpaschke@gmail.com>
2021-03-03 03:39:15 +0000

    submitting task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all names of a single genre
+SELECT tv_shows.title
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
