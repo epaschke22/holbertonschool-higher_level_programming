9a56b896d2fb2ad45f2f3e057d4d1f8bd0585d83
epaschke22 <ericksonpaschke@gmail.com>
2021-03-03 03:34:21 +0000

    updating task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,9 @@
 -- Lists all genres of a single show
 SELECT tv_genres.name
-FROM tv_show_genres LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = 8;
+FROM tv_shows 
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
