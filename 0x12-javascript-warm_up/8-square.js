f3582955185567de2511835c2477620a18bca3dd
epaschke22 <ericksonpaschke@gmail.com>
2021-03-22 19:29:00 +0000

    updating task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (!isNaN(parseInt(process.argv[2]))) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('X'.repeat(parseInt(process.argv[2])));
   }
