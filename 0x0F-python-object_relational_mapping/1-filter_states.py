6676b39f6b8362f69d08311120bfa4f644ad2b7d
epaschke22 <ericksonpaschke@gmail.com>
2021-03-25 19:03:51 +0000

    submitting task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all states from a database"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == 'N'
+            print(row)
+    cur.close()
+    conn.close()
