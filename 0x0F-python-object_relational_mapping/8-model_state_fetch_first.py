ef66588af6cd61a565241e4fe834b20f3b8322e7
epaschke22 <ericksonpaschke@gmail.com>
2021-03-27 16:39:06 +0000

    updating task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,6 +13,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    try:
+        state = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(state.id, state.name))
+    except:
+        print("Nothing")
     session.close()
