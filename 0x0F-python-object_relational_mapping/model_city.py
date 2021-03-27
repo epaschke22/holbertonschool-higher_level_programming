60b6faa7db6e4fce341e457d6ce5a40b4434abbf
epaschke22 <ericksonpaschke@gmail.com>
2021-03-27 14:52:26 +0000

    adding both model files

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""City Class"""
+import sys
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """City SQLAlchemy class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
