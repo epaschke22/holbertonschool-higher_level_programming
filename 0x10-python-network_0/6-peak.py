5faf77b1160afc4020ef8d685a17dcaaa2ff4820
epaschke22 <ericksonpaschke@gmail.com>
2021-04-08 20:27:31 +0000

    updating task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,5 +14,6 @@ def find_peak(list_of_integers):
     if list_of_integers[-1] >= list_of_integers[-2]:
         return list_of_integers[-1]
     for i in range(1, n - 1):
-        if (list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]):
+        if (list_of_integers[i] >= list_of_integers[i - 1] and
+           list_of_integers[i] >= list_of_integers[i + 1]):
             return list_of_integers[i]
