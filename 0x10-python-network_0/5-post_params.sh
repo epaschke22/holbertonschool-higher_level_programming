5c2d1a2f7d3fe21a2179895f23f9e804dab59907
epaschke22 <ericksonpaschke@gmail.com>
2021-04-08 19:49:38 +0000

    updating task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sets header variables with curl and uses POST
-curl $1 -s -X POST -H "email: hr@holbertonschool.com" -H "subject: I will always be here for PLD"
+curl $1 -s -X POST -d "email: hr@holbertonschool.com" -d "subject: I will always be here for PLD"
