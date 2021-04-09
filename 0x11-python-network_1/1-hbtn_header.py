eca4837b79f8d63c2b863fd8c458d3316f4202bd
epaschke22 <ericksonpaschke@gmail.com>
2021-04-09 19:36:09 +0000

    adding if name == main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ from urllib import request
 from sys import argv
 
 
-with request.urlopen(argv[1]) as response:
-    html = response.info()
-    print(html['X-Request-Id'])
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as response:
+        html = response.info()
+        print(html['X-Request-Id'])
