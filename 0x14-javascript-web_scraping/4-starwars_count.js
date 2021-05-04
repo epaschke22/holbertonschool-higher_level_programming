64f970df08267790b59cd6737276efeb8b653b96
epaschke22 <ericksonpaschke@gmail.com>
2021-05-04 18:53:03 +0000

    updating task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 request.get({ url: process.argv[2] }, function (error, response, body) {
-  if (error) console.log(error);
+  if (error) console.error(error);
   let count = 0;
   const titlelist = JSON.parse(body).results;
   for (let i = 0; i < titlelist.length; i++) {
